c7eeb2a5eac82ca1b25cacaa25902b7580ed365b
stevenjchang <stevenjchang@gmail.com>
2020-07-29 17:08:27 -0700

    fix(): null check for if .env vars don't exist
    - otherwise app will completely error out

--- src/Auth.js
+++ src/Auth.js
@@ -6,16 +6,25 @@ export const AuthContext = React.createContext();
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [pending, setPending] = useState(true);
+  const [firebaseKeyMissingMsg, setFirebaseKeyMissingMsg] = useState(false);
 
   useEffect(() => {
-    app.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      setPending(false);
-    });
+    if (process.env.REACT_APP_FIREBASE_KEY) {
+      app.auth().onAuthStateChanged((user) => {
+        setCurrentUser(user);
+        setPending(false);
+      });
+    } else {
+      setFirebaseKeyMissingMsg(true);
+    }
   }, []);
 
-  if (pending) {
+  if (pending && !firebaseKeyMissingMsg) {
     return <>Loading...</>;
+  }
+
+  if (firebaseKeyMissingMsg) {
+    return <>Firebase KEYs are missing, please add keys to .env</>;
   }
 
   return (
